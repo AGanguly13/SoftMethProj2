a1057a7af770be78e2d9c983b7e27490470e7451
Kennan Guan <kennanguan722@gmail.com>
2022-09-26 13:55:00 -0400

    Added Month enum

---
+++ src/Month.java
@@ -0,0 +1,24 @@
+public enum Month {
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER(31),
+    NOVEMBER(30),
+    DECEMBER(31);
+
+    private int days;
+
+    public int getDays() {
+        return this.days;
+    }
+
+    private Month(int days){
+        this.days = days;
+    }
+}
