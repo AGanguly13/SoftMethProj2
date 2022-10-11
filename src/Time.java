3e1eced4dab4823ebaf41216fea60d44a8dc8dce
Kennan Guan <kennanguan722@gmail.com>
2022-10-11 12:17:48 -0400

    Updated Time

--- src/Time.java
+++ src/Time.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 /**
  * Defines the times of the set fitness classes as morning and afternoon fitness class sessions.
  * Time is defined by hour and minutes and also a textual representation in a clock format.
@@ -6,23 +7,20 @@
  * @author Kennan Guan, Adwait Ganguly
  */
 public enum Time {
-    MORNING(9, 30, "9:30"),
-    AFTERNOON(14,00, "14:00");
+    MORNING(9, 30),
+    AFTERNOON(14,00);
 
     private final int hour;
     private final int minute;
-    private final String clock;
 
     /**
      * Constructs the Time object with hours, minutes, and a string representation.
      * @param hour the hour of day a class takes place.
      * @param minute the minutes into an hour a class takes place.
-     * @param clock is a string representation of time instance
      */
-    Time (int hour, int minute, String clock) {
+    Time (int hour, int minute) {
         this.hour = hour;
         this.minute = minute;
-        this.clock = clock;
     }
 
     /**
@@ -46,6 +44,7 @@ public enum Time {
      * @return the clock representation.
      */
     public String getClock() {
-        return clock;
+        DecimalFormat formatted = new DecimalFormat("#00");
+        return hour + formatted.format(minute);
     }
 }
